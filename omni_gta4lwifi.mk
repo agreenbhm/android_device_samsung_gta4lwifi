a660b084d2a4cc528c0bf0de50382ade3eec63e3
Drew Green <agreenbhm@gmail.com>
2021-06-23 01:12:19 +0000

    Removed unnecessary fstab, updated boardconfig for proper screen rotation, and adjusted omni_gta4lwifi to add similar settings to known working configs

--- omni_gta4lwifi.mk
+++ omni_gta4lwifi.mk
@@ -16,23 +16,32 @@
 # limitations under the License.
 #
 
-# Inherit from those products. Most specific first.
+PRODUCT_RELEASE_NAME := gta4lwifi
+
+$(call inherit-product, vendor/omni/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# Inherit from gta4lwifi device
 $(call inherit-product, device/samsung/gta4lwifi/device.mk)
 
+# Charger
+PRODUCT_PACKAGES += \
+    charger_res_images
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/gta4lwifi/recovery/root,recovery/root)
+
+# Inherit from those products. Most specific first.
+#$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+
+# Inherit from gta4lwifi device
+
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+#$(call inherit-product, vendor/omni/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta4lwifi
 PRODUCT_NAME := omni_gta4lwifi
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := samsung
+PRODUCT_MODEL := SM-T500
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_RELEASE_NAME := samsung samsung
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
