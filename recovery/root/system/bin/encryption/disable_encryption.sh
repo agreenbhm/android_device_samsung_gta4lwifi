d06bc402ac623703e60f6ea41370955a1c127d82
agreenbhm <agreenbhm@gmail.com>
2022-03-28 12:50:39 +0000

    Updated encrypt/decrypt scripts.

--- recovery/root/system/bin/encryption/disable_encryption.sh
+++ recovery/root/system/bin/encryption/disable_encryption.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+echo "Disabling force-encrypt"
 blockdev --setrw /dev/block/mapper/vendor
 mount -t ext4 -o rw /dev/block/mapper/vendor /mnt/vendor
 cp /system/bin/encryption/fstab.default.decrypt /mnt/vendor/etc/fstab.default
 cp /system/bin/encryption/fstab.emmc.decrypt /mnt/vendor/etc/fstab.emmc
 umount /mnt/vendor
+echo "Done!  Now format data and reboot to ROM."
--- recovery/root/system/bin/encryption/enable_encryption.sh
+++ recovery/root/system/bin/encryption/enable_encryption.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+echo "Enabling force-encrypt"
 blockdev --setrw /dev/block/mapper/vendor
 mount -t ext4 -o rw /dev/block/mapper/vendor /mnt/vendor
 cp /system/bin/encryption/fstab.default.encrypt /mnt/vendor/etc/fstab.default
 cp /system/bin/encryption/fstab.emmc.encrypt /mnt/vendor/etc/fstab.emmc
 umount /mnt/vendor
+echo "Done!  Now format data and reboot to ROM."
